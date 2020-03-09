55cb9a787e8beb30c4bad470df78ceb6401c0526
arberk6 <arkadriu@gmail.com>
2020-03-09 23:57:47 +0100

    first commit

---
+++ BlockChainApp/BlockChainApp/Blocks/Tracker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace BlockChainApp.Blocks
+{
+    public class Tracker
+    {
+        public static void Main()
+        {
+            string path = @"C:\Users\arkad\Desktop\block";
+
+            try
+            {
+
+                //to be continued
+                //changed files tracker
+                var fi = new DirectoryInfo(path);
+
+                while (true)
+                {
+                    fi.Refresh();
+                    DateTime dt = fi.LastAccessTime; 
+                    DateTime f1 = fi.LastWriteTime;
+                    Console.WriteLine("test");
+                    Console.Write(true);
+                }
+                //
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+
+        }
+    }
+}
